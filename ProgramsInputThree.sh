c00eca988658c3f2d9e84190933bbea70c28aac4
shuchitasingh <singhshuchita5@gmail.com>
2021-03-21 18:27:39 +0530

    Added:Compute_c+a/b

--- ProgramsInputThree.sh
+++ ProgramsInputThree.sh
@@ -28,3 +28,17 @@ operation1=$(($number1+$number2*$number3))
 operation2=$(($number1*$number2+$number3))
 echo "First operation is $operation1 "
 echo "Second operation is $operation2 "
+
+
+
+#USERINPUT
+read -p "Enter value for a " number1
+read -p "Enter value for b " number2
+read -p "Enter value for c " number3
+
+#ARITHMETIC OPERATIONS
+operation1=$(($number1+$number2*$number3))
+operation2=$(($number1*$number2+$number3))
+operation3=`echo "scale=2;$number3+$number1/$number2" | bc`
+echo "Operations are $operation1 $operation2 $operation3"
+
