be26979aa618ccf7ab21fac92760c82d2a253b0a
shuchitasingh <singhshuchita5@gmail.com>
2021-03-21 12:57:23 +0530

    Added : Takes three inputs

--- ProgramsInputThree.sh
+++ ProgramsInputThree.sh
@@ -1,2 +1,8 @@
 =============Welcome to Arithmetic Computation=================
 
+#!/bin/bash
+
+#USERINPUT
+read -p "Enter value for a " number1
+read -p "Enter value for b " number2
+read -p "Enter value for c " number3
