3986af2331d84133d96a929ec5274a43de1dcaf5
shuchitasingh <singhshuchita5@gmail.com>
2021-03-21 18:19:56 +0530

    Added Airthmetic_problem

--- ProgramsInputThree.sh
+++ ProgramsInputThree.sh
@@ -15,3 +15,16 @@ read -p "Enter value for c " number3
 #ARITHMETIC OPERATIONS
 operation1=$(($number1+$number2*$number3))
 echo "First operation is $operation1 "
+
+
+
+#USERINPUT
+read -p "Enter value for a " number1
+read -p "Enter value for b " number2
+read -p "Enter value for c " number3
+
+#ARITHMETIC OPERATIONS
+operation1=$(($number1+$number2*$number3))
+operation2=$(($number1*$number2+$number3))
+echo "First operation is $operation1 "
+echo "Second operation is $operation2 "
