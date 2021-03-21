06593b73ae86327b94cfba661253f0815fa9278a
shuchitasingh <singhshuchita5@gmail.com>
2021-03-21 13:10:32 +0530

    Added: Arithmetic operation

--- ProgramsInputThree.sh
+++ ProgramsInputThree.sh
@@ -6,3 +6,12 @@
 read -p "Enter value for a " number1
 read -p "Enter value for b " number2
 read -p "Enter value for c " number3
+
+#USERINPUT
+read -p "Enter value for a " number1
+read -p "Enter value for b " number2
+read -p "Enter value for c " number3
+
+#ARITHMETIC OPERATIONS
+operation1=$(($number1+$number2*$number3))
+echo "First operation is $operation1 "
