a036d2907e245cbb35be4659c0003bad79bc21e1
shuchitasingh <singhshuchita5@gmail.com>
2021-03-22 13:30:43 +0530

    Added :Dict to array

--- ProgramsInputThree.sh
+++ ProgramsInputThree.sh
@@ -74,3 +74,30 @@ arithmetic[operation1]=$operation1
 arithmetic[operation2]=$operation2
 arithmetic[operation3]=$operation3
 arithmetic[operation4]=$operation4
+
+
+#USERINPUT
+read -p "Enter value for a " number1
+read -p "Enter value for b " number2
+read -p "Enter value for c " number3
+
+#ARITHMETIC OPERATIONS
+operation1=$(($number1+$number2*$number3))
+operation2=$(($number1*$number2+$number3))
+operation3=`echo "scale=2;$number3+$number1/$number2" | bc`
+operation4=`echo "scale=2; $number1%$number2+$number3" | bc`
+echo "Operations are $operation1 $operation2 $operation3 $operation4 "
+
+#STORING OPERATIONS IN DICTIONARY
+declare -A arithmetic
+arithmetic[operation1]=$operation1
+arithmetic[operation2]=$operation2
+arithmetic[operation3]=$operation3
+arithmetic[operation4]=$operation4
+
+#STORING THE OPERATIONS IN AN ARRAY
+for (( index=0; index<${#arithmetic[@]}; index++ ))
+do
+   array[index]=${arithmetic[operation$((index+1))]}
+done
+echo ${array[@]}
